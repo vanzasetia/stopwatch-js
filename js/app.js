0924dd3080f7fb96af8ac05f8f818269b5290a9e
Vanza Setia <venusbumi2@gmail.com>
2021-07-20 14:47:19 +0700

    Select all elements

---
+++ js/app.js
@@ -0,0 +1,9 @@
+// DOM ELEMENTS
+const themeToggle = document.querySelector('.js-themeToggle')
+const minutesNumber = document.querySelector('.js-minutesNumber')
+const secondsNumber = document.querySelector('.js-secondsNumber')
+const milisecondsNumber = document.querySelector('.js-milisecondsNumber')
+const startBtn = document.querySelector('.js-startBtn')
+const stopBtn = document.querySelector('.js-stopBtn')
+const resetBtn = document.querySelector('.js-resetBtn')
+const currentYear = document.querySelector('.js-currentYear')
