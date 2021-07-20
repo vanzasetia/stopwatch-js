38bccad4a73435cfce8bb1105d97854d8812f7dd
Vanza Setia <venusbumi2@gmail.com>
2021-07-20 15:35:50 +0700

    Start stopwatch functionality work!

--- js/app.js
+++ js/app.js
@@ -7,3 +7,60 @@ const startBtn = document.querySelector('.js-startBtn')
 const stopBtn = document.querySelector('.js-stopBtn')
 const resetBtn = document.querySelector('.js-resetBtn')
 const currentYear = document.querySelector('.js-currentYear')
+
+// VARIABLES
+let timerOn = false
+let miliseconds = 0
+let seconds = 0
+let minutes = 0
+let milisecondsTimer
+let secondsTimer
+let minutesTimer
+
+// ADD EVENT LISTENERS
+document.addEventListener('DOMContentLoaded', getYear)
+startBtn.addEventListener('click', startStopwatch)
+
+
+// FUNCTIONS
+function getYear() {
+  currentYear.textContent = new Date().getFullYear()
+}
+
+function startStopwatch() {
+  if(!timerOn) {
+    timerOn = true
+    countUp()
+  }
+}
+
+function countUp() {
+  milisecondsCount()
+}
+
+function milisecondsCount() {
+  milisecondsNumber.textContent = addZero(miliseconds)
+  miliseconds++
+  milisecondsTimer = setTimeout(countUp, 10);
+  
+  if (miliseconds > 99) {
+    miliseconds = 0
+    seconds++
+    secondsNumber.textContent = addZero(seconds)
+  }
+  
+  if (seconds > 59) {
+    seconds = 0
+    secondsNumber.textContent = addZero(seconds)
+    minutes++
+    minutesNumber.textContent = addZero(minutes)
+  }
+}
+
+function addZero(number) {
+  if (number < 10) {
+    return `0${number}`
+  } else {
+    return number
+  }
+}
