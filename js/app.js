10951e10c2caef763f48dd82a632f85b22ac813c
Vanza Setia <venusbumi2@gmail.com>
2021-07-22 10:48:47 +0700

    Functionality done

--- js/app.js
+++ js/app.js
@@ -1,107 +1,102 @@
-// DOM ELEMENTS
-const themeToggle = document.querySelector('.js-themeToggle')
-const minutesNumber = document.querySelector('.js-minutesNumber')
-const secondsNumber = document.querySelector('.js-secondsNumber')
-const milisecondsNumber = document.querySelector(
-  '.js-milisecondsNumber'
-)
-const startBtn = document.querySelector('.js-startBtn')
-const stopBtn = document.querySelector('.js-stopBtn')
-const resetBtn = document.querySelector('.js-resetBtn')
-const currentYear = document.querySelector('.js-currentYear')
-
-// VARIABLES
-let timerOn = false
-let miliseconds = 0
-let seconds = 0
-let minutes = 0
-let milisecondsTimer
-
-// ADD EVENT LISTENERS
-document.addEventListener('DOMContentLoaded', getYear)
-startBtn.addEventListener('click', startStopwatch)
-stopBtn.addEventListener('click', stopStopwatch)
-resetBtn.addEventListener('click', resetStopwatch)
-themeToggle.addEventListener('click', toggleDark)
-
-// FUNCTIONS
-
-// TOGGLE DARK THEME
-function toggleDark() {
-  document.body.classList.toggle('dark')
-}
-
-// Get Current Year
-function getYear() {
-  currentYear.textContent = new Date().getFullYear()
-}
-
-// Reset Stopwatch
-function resetStopwatch() {
-  clearTimeout(milisecondsTimer)
-  timerOn = false
-
-  miliseconds = 0
-  seconds = 0
-  minutes = 0
-
-  milisecondsNumber.textContent = addZero(miliseconds)
-  secondsNumber.textContent = addZero(seconds)
-  minutesNumber.textContent = addZero(minutes)
-
-  startBtn.disabled = false
-  stopBtn.disabled = true
-  resetBtn.disabled = true
-}
-
-// Stop Stopwatch
-function stopStopwatch() {
-  clearTimeout(milisecondsTimer)
-  timerOn = false
-
-  startBtn.disabled = false
-  stopBtn.disabled = true
-}
-
-// Start Stopwatch
-function startStopwatch() {
-  if (!timerOn) {
-    timerOn = true
-    countUp()
+'use strict';
+(function () {
+  // ELEMENTS
+  const themeToggle = document.querySelector('.js-themeToggle')
+  const minutesDOM = document.querySelector('.js-minutesDOM')
+  const secondsDOM = document.querySelector('.js-secondsDOM')
+  const milisecondsDOM = document.querySelector('.js-milisecondsDOM')
+  const startBtn = document.querySelector('.js-startBtn')
+  const pauseBtn = document.querySelector('.js-pauseBtn')
+  const resetBtn = document.querySelector('.js-resetBtn')
+  const year = document.querySelector('.js-year')
+  
+  // VARIABLES
+  let stopwatchStart = false
+  let miliseconds = 0
+  let seconds = 0
+  let minutes = 0
+  let milisecondsTimer
+  
+  // ADD EVENT LISTENERS
+  document.addEventListener('DOMContentLoaded', getYear)
+  startBtn.addEventListener('click', startStopwatch)
+  pauseBtn.addEventListener('click', pauseStopwatch)
+  resetBtn.addEventListener('click', resetStopwatch)
+  themeToggle.addEventListener('click', toggleDark)
+  
+  // FUNCTIONS
+  function toggleDark() {
+    document.body.classList.toggle('dark')
   }
-
-  startBtn.disabled = true
-  stopBtn.disabled = false
-  resetBtn.disabled = false
-}
-
-function countUp() {
-  milisecondsCount()
-}
-
-function milisecondsCount() {
-  milisecondsNumber.textContent = addZero(miliseconds)
-  miliseconds++
-  milisecondsTimer = setTimeout(countUp, 10)
-
-  if (miliseconds > 99) {
+  
+  function startStopwatch() {
+    if (!stopwatchStart) {
+      stopwatchStart = true
+      countUp()
+    }
+  
+    startBtn.disabled = true
+    pauseBtn.disabled = false
+    resetBtn.disabled = false
+  }
+  
+  function countUp() {
+    milisecondsCount()
+  }
+  
+  function milisecondsCount() {
+    milisecondsDOM.textContent = addZero(miliseconds)
+    miliseconds++
+    milisecondsTimer = setTimeout(countUp, 10)
+  
+    if (miliseconds > 99) {
+      miliseconds = 0
+      seconds++
+      secondsDOM.textContent = addZero(seconds)
+    }
+  
+    if (seconds > 59) {
+      seconds = 0
+      secondsDOM.textContent = addZero(seconds)
+      minutes++
+      minutesDOM.textContent = addZero(minutes)
+    }
+  }
+  
+  function addZero(number) {
+    if (number < 10) {
+      return `0${number}`
+    } else {
+      return number
+    }
+  }
+  
+  function pauseStopwatch() {
+    clearTimeout(milisecondsTimer)
+    stopwatchStart = false
+  
+    startBtn.disabled = false
+    pauseBtn.disabled = true
+  }
+  
+  function resetStopwatch() {
+    clearTimeout(milisecondsTimer)
+    stopwatchStart = false
+  
     miliseconds = 0
-    seconds++
-    secondsNumber.textContent = addZero(seconds)
-  }
-
-  if (seconds > 59) {
     seconds = 0
-    secondsNumber.textContent = addZero(seconds)
-    minutes++
-    minutesNumber.textContent = addZero(minutes)
+    minutes = 0
+  
+    milisecondsDOM.textContent = addZero(miliseconds)
+    secondsDOM.textContent = addZero(seconds)
+    minutesDOM.textContent = addZero(minutes)
+  
+    startBtn.disabled = false
+    pauseBtn.disabled = true
+    resetBtn.disabled = true
   }
-}
-
-function addZero(number) {
-  if (number < 10) {
-    return `0${number}`
-  } else {
-    return number
+  
+  function getYear() {
+  year.textContent = new Date().getFullYear()
   }
-}
+})()
--- js/refactor.js
+++ js/refactor.js
@@ -19,8 +19,83 @@
   
   // ADD EVENT LISTENERS
   document.addEventListener('DOMContentLoaded', getYear)
+  startBtn.addEventListener('click', startStopwatch)
+  pauseBtn.addEventListener('click', pauseStopwatch)
+  resetBtn.addEventListener('click', resetStopwatch)
+  themeToggle.addEventListener('click', toggleDark)
   
   // FUNCTIONS
+  function toggleDark() {
+    document.body.classList.toggle('dark')
+  }
+  
+  function startStopwatch() {
+    if (!stopwatchStart) {
+      stopwatchStart = true
+      countUp()
+    }
+  
+    startBtn.disabled = true
+    pauseBtn.disabled = false
+    resetBtn.disabled = false
+  }
+  
+  function countUp() {
+    milisecondsCount()
+  }
+  
+  function milisecondsCount() {
+    milisecondsDOM.textContent = addZero(miliseconds)
+    miliseconds++
+    milisecondsTimer = setTimeout(countUp, 10)
+  
+    if (miliseconds > 99) {
+      miliseconds = 0
+      seconds++
+      secondsDOM.textContent = addZero(seconds)
+    }
+  
+    if (seconds > 59) {
+      seconds = 0
+      secondsDOM.textContent = addZero(seconds)
+      minutes++
+      minutesDOM.textContent = addZero(minutes)
+    }
+  }
+  
+  function addZero(number) {
+    if (number < 10) {
+      return `0${number}`
+    } else {
+      return number
+    }
+  }
+  
+  function pauseStopwatch() {
+    clearTimeout(milisecondsTimer)
+    stopwatchStart = false
+  
+    startBtn.disabled = false
+    pauseBtn.disabled = true
+  }
+  
+  function resetStopwatch() {
+    clearTimeout(milisecondsTimer)
+    stopwatchStart = false
+  
+    miliseconds = 0
+    seconds = 0
+    minutes = 0
+  
+    milisecondsDOM.textContent = addZero(miliseconds)
+    secondsDOM.textContent = addZero(seconds)
+    minutesDOM.textContent = addZero(minutes)
+  
+    startBtn.disabled = false
+    pauseBtn.disabled = true
+    resetBtn.disabled = true
+  }
+  
   function getYear() {
   year.textContent = new Date().getFullYear()
   }
